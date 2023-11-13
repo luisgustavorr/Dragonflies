84e225cc0a73966289a6f79e067c394cb06a2759
luisgustavorr <luisgustavo20061@gmail.com>
2023-11-13 18:27:33 -0300

    UPDATE

--- websocket-server.js
+++ websocket-server.js
@@ -18,11 +18,8 @@ let transporter = nodemailer.createTransport({
 // Middleware para fazer o parsing da query string (req.query)
 app.use(express.urlencoded({ extended: true }));
 
-// Rota para lidar com requisições GET
-app.get('/monitorar-get', (req, res) => {
-    const data = req.query;
-    console.log('Requisição GET recebida:', data);
-    
+// Função assíncrona para envio de e-mails
+const enviarEmail = async (data) => {
     const now = new Date();
     const year = now.getFullYear();
     const month = ("0" + (now.getMonth() + 1)).slice(-2);
@@ -30,23 +27,34 @@ app.get('/monitorar-get', (req, res) => {
     const hour = ("0" + now.getHours()).slice(-2);
     const minute = ("0" + now.getMinutes()).slice(-2);
     const second = ("0" + now.getSeconds()).slice(-2);
-    
+
     const diaFormatado = `${day}-${month}-${year} `;
     const horaFormatada = `${hour}:${minute}:${second}`;
 
-    // Aqui você pode realizar qualquer lógica desejada com os dados da requisição
-    transporter.sendMail({
-        from: 'Super Error Log <luisgustavo20061@gmail.com>',
-        to: data.Email,
-        subject: "Erro no sistema",
-        html: `O sistema "${data.Sistema}" apresentou o seguinte erro: ${data.Error}<br>Data: ${diaFormatado} às ${horaFormatada}`,
-    }).then(message => {
-        console.log(message);
-    }).catch(err => {
-        console.log(err);
-    });
+    try {
+        // Envia o e-mail
+        const message = await transporter.sendMail({
+            from: 'Super Error Log <luisgustavo20061@gmail.com>',
+            to: data.Email,
+            subject: "Erro no sistema",
+            html: `O sistema "${data.Sistema}" apresentou o seguinte erro: ${data.Error}<br>Data: ${diaFormatado} às ${horaFormatada}`,
+        });
 
-    res.json({ status: `Recebido com sucesso ${data.Email}-${data.Sistema}-${data.Error}-${diaFormatado}-${horaFormatada}` });
+        console.log(message);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+// Rota para lidar com requisições GET
+app.get('/monitorar-get', async (req, res) => {
+    const data = req.query;
+    console.log('Requisição GET recebida:', data);
+
+    // Chama a função assíncrona para enviar o e-mail
+    await enviarEmail(data);
+
+    res.json({ status: `Recebido com sucesso ` });
 });
 
 // Inicia o servidor na porta especificada

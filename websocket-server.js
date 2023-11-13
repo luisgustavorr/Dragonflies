9be6879b662ec3d6774c01bbca9ef437a6262824
luisgustavorr <luisgustavo20061@gmail.com>
2023-11-13 14:41:40 -0300

    Creating

---
+++ websocket-server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require("nodemailer");
+const app = express();
+const port = 3000;
+
+let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com", // replace with your email service
+    port:587,
+    secure: false,
+    auth: {
+        user:"luisgustavo20061@gmail.com",
+        pass:"lmwr xead llux nuye"
+    }
+})
+
+
+// Middleware para fazer o parsing do corpo da requisição (req.body)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Rota para lidar com requisições POST
+
+app.post('/monitorar-post', (req, res) => {
+    const data = req.body;
+    console.log('Requisição POST recebida:', data);
+    const now = new Date();
+
+    const year = now.getFullYear();
+    const month = ("0" + (now.getMonth() + 1)).slice(-2);
+    const day = ("0" + now.getDate()).slice(-2);
+    
+    const hour = ("0" + now.getHours()).slice(-2);
+    const minute = ("0" + now.getMinutes()).slice(-2);
+    const second = ("0" + now.getSeconds()).slice(-2);
+    const diaFormatado = `${day}-${month}-${year} `;
+    const horaFormatada = `${hour}:${minute}:${second}`;
+    // Aqui você pode realizar qualquer lógica desejada com os dados da requisição
+    transporter.sendMail({
+        from:'Super Error Log <luisgustavo20061@gmail.com>',
+        to:data.Email,
+        subject:"Erro no sistema",
+        text:`O sistema "${data.Sistema}" apresentou o seguinte erro: ${data.Erro} no dia ${diaFormatado} as ${horaFormatada}`,
+        // html:"confira o erro no seguinte <a href='google.com'> aqui </a>"
+    }).then(message => {
+        console.log(message);
+    }).catch(err=> {
+        console.log(err)
+    });
+    res.json({ status: 'Recebido com sucesso' });
+});
+
+// Inicia o servidor na porta especificada
+const server = app.listen(port, () => {
+    console.log(`Servidor escutando na porta ${port}`);
+});
+
+// Monitora eventos de fechamento do servidor
+server.on('close', () => {
+    console.log('Servidor fechado');
+});
